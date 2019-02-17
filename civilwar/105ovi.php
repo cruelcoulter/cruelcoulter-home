2fe90aa2f7e74ac18b3878e419e69e64748e95e6
Ron Coulter <ron@cruelcoulter.com>
2019-02-17 14:01:23 -0500

    Version 1 with no links. Ready for upload.

--- civilwar/105ovi.php
+++ civilwar/105ovi.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
 <html lang="en"><!-- updated for Bootstrap 3 -->
-<head>    <title>The 105th Ohio Volunteer Infantry (OVI)</title>
+<head>    
+  <title>The 105th Ohio Volunteer Infantry (OVI)</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta charset="utf-8" />    <!-- Bootstrap core CSS -->
+  <meta charset="utf-8" />    
+  <!-- Bootstrap core CSS -->
   <link href="bootstrap-3.0.0/dist/css/bootstrap.css" rel="stylesheet">
 
-        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->    <!--[if lt IE 9]>      <script src="bootstrap-3.0.0/assets/js/html5shiv.js"></script>      <script src="bootstrap-3.0.0/assets/js/respond.min.js"></script>    <![endif]-->    <!-- Fav and touch icons --><link rel="apple-touch-icon-precomposed" sizes="144x144" href="bootstrap-3.0.0/assets/ico/apple-touch-icon-144-precomposed.png" ><link rel="apple-touch-icon-precomposed" sizes="114x114" href="bootstrap-3.0.0/assets/ico/apple-touch-icon-114-precomposed.png">
+        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+            <script src="bootstrap-3.0.0/assets/js/html5shiv.js"></script>
+            <script src="bootstrap-3.0.0/assets/js/respond.min.js"></script>    
+        <![endif]-->    
+        <!-- Fav and touch icons -->
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="bootstrap-3.0.0/assets/ico/apple-touch-icon-144-precomposed.png" ><link rel="apple-touch-icon-precomposed" sizes="114x114" href="bootstrap-3.0.0/assets/ico/apple-touch-icon-114-precomposed.png">
         <link rel="apple-touch-icon-precomposed" sizes="72x72" href="bootstrap-3.0.0/assets/ico/apple-touch-icon-72-precomposed.png">
         <link rel="apple-touch-icon-precomposed" href="bootstrap-3.0.0/assets/ico/apple-touch-icon-57-precomposed.png">
         <link rel="shortcut icon" href="../includes/favicon.png">
@@ -13,15 +21,65 @@
       <body>
         <?php include 'navbar.php';?>
           <div class="container">
-            <h2>
-            <ul>
-              <li>Letter written <a href="Letter1.php">May 21, 1863, from Murfreesboro, TN</a></li>
-              <li>Letter written <a href="Letter2.php">December 19, 1863, from Chattanooga, TN</a></li>
-              <li>Letter written <a href="Letter3.php">December 21, 1863, from Chattanooga, TN</a></li>
-              <li>Letter written <a href="Letter4.php">December 21, 1864, from Savannah, GA</a></li>
-              <li>Letter written <a href="Letter5.php">June 15, 1865, from Girard, PA</a></li>
-            </ul>
-        </div><!--/.container -->
+          <p>The 105th Ohio Volunteer Infantry (or 105th OVI) was an infantry regiment in the Union Army during the American Civil War.</p>
+            <div class="row">
+              <div class="column">Unit</div>
+              <div class="column">Aug-Sep 1862</div>
+              <div class="column">Sep-Nov 1862</div>
+              <div class="column">Nov 1862-Jan 1863</div>
+              <div class="column">Jan 1863-June 1863</div>
+              <div class="column">June 1863-Oct 1863</div>
+              <div class="column">Oct 1863-July 1865</div>
+            </div>
+            <div class="row">
+              <div class="column">Brigade</div>
+              <div class="column">33rd - William Terrill</div>
+              <div class="column">33rd - William Terrill</div>
+              <div class="column">1st</div>
+              <div class="column">1st</div>
+              <div class="column">2nd</div>
+              <div class="column">2nd</div>
+            </div>
+            <div class="row">
+              <div class="column">Division</div>
+              <div class="column">10th - James S. Jackson</div>
+              <div class="column">10th - James S. Jackson</div>
+              <div class="column">5th</div>
+              <div class="column">5th</div>
+              <div class="column">4th</div>
+              <div class="column">3rd</div>
+            </div>
+            <div class="row">
+              <div class="column">Corps</div>
+              <div class="column">II</div>
+              <div class="column">II</div>
+              <div class="column">Center, XIV</div>
+              <div class="column">XIV</div>
+              <div class="column">XIV</div>
+              <div class="column">XIV</div>
+            </div>
+            <div class="row">
+              <div class="column">Army</div>
+              <div class="column">Army of the Ohio</div>
+              <div class="column">Army of the Ohio</div>
+              <div class="column">Army of the Cumberland</div>
+              <div class="column">Army of the Cumberland</div>
+              <div class="column">Army of the Cumberland</div>
+              <div class="column">Army of the Cumberland</div>
+            </div>
+            <div class="row">
+              <div class="column">Significant events</div>
+              <div class="column">Mustered in August 20, 1862 under command of Col. Albert S. Hall</div>
+              <div class="column">Battle of Perryville, KY, October 1862</div>
+              <div class="column">&nbsp;</div>
+              <div class="column">Stationed in Murfreeesboro, TN</div>
+              <div class="column">Battle of Chickamauga, September 1863</div>
+              <div class="column">Seige of Chattanooga; battle of Atlanta; march to the sea</div>
+            </div>
+
+
+
+          </div><!--/.container -->
                 <?php include 'footer.php';?>
                 <!-- Bootstrap core JavaScript    ================================================== -->
                 <!-- Placed at the end of the document so the pages load faster -->
