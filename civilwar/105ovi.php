03f7b057a5f3d0406df83366a2c609d8b32f0e9b
Ron Coulter <ron@cruelcoulter.com>
2019-02-17 14:29:37 -0500

    Updated for Bootstrap 4

--- civilwar/105ovi.php
+++ civilwar/105ovi.php
@@ -2,11 +2,11 @@
 <html lang="en"><!-- updated for Bootstrap 3 -->
 <head>    
   <title>The 105th Ohio Volunteer Infantry (OVI)</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta charset="utf-8" />    
-  <!-- Bootstrap core CSS -->
-  <link href="bootstrap-3.0.0/dist/css/bootstrap.css" rel="stylesheet">
-
+  <!-- Bootstrap core CSS. Changed to v4 CDN 
+  <link href="bootstrap-3.0.0/dist/css/bootstrap.css" rel="stylesheet">-->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!--[if lt IE 9]>
             <script src="bootstrap-3.0.0/assets/js/html5shiv.js"></script>
@@ -20,70 +20,73 @@
       </head>
       <body>
         <?php include 'navbar.php';?>
-          <div class="container">
+          <div class="container-fluid">
           <p>The 105th Ohio Volunteer Infantry (or 105th OVI) was an infantry regiment in the Union Army during the American Civil War.</p>
             <div class="row">
-              <div class="column">Unit</div>
-              <div class="column">Aug-Sep 1862</div>
-              <div class="column">Sep-Nov 1862</div>
-              <div class="column">Nov 1862-Jan 1863</div>
-              <div class="column">Jan 1863-June 1863</div>
-              <div class="column">June 1863-Oct 1863</div>
-              <div class="column">Oct 1863-July 1865</div>
+              <div class="col">Unit</div>
+              <div class="col">Aug-Sep 1862</div>
+              <div class="col">Sep-Nov 1862</div>
+              <div class="col">Nov 1862-Jan 1863</div>
+              <div class="col">Jan 1863-June 1863</div>
+              <div class="col">June 1863-Oct 1863</div>
+              <div class="col">Oct 1863-July 1865</div>
             </div>
             <div class="row">
-              <div class="column">Brigade</div>
-              <div class="column">33rd - William Terrill</div>
-              <div class="column">33rd - William Terrill</div>
-              <div class="column">1st</div>
-              <div class="column">1st</div>
-              <div class="column">2nd</div>
-              <div class="column">2nd</div>
+              <div class="col">Brigade</div>
+              <div class="col">33rd - William Terrill</div>
+              <div class="col">33rd - William Terrill</div>
+              <div class="col">1st</div>
+              <div class="col">1st</div>
+              <div class="col">2nd</div>
+              <div class="col">2nd</div>
             </div>
             <div class="row">
-              <div class="column">Division</div>
-              <div class="column">10th - James S. Jackson</div>
-              <div class="column">10th - James S. Jackson</div>
-              <div class="column">5th</div>
-              <div class="column">5th</div>
-              <div class="column">4th</div>
-              <div class="column">3rd</div>
+              <div class="col">Division</div>
+              <div class="col">10th - James S. Jackson</div>
+              <div class="col">10th - James S. Jackson</div>
+              <div class="col">5th</div>
+              <div class="col">5th</div>
+              <div class="col">4th</div>
+              <div class="col">3rd</div>
             </div>
             <div class="row">
-              <div class="column">Corps</div>
-              <div class="column">II</div>
-              <div class="column">II</div>
-              <div class="column">Center, XIV</div>
-              <div class="column">XIV</div>
-              <div class="column">XIV</div>
-              <div class="column">XIV</div>
+              <div class="col">Corps</div>
+              <div class="col">II</div>
+              <div class="col">II</div>
+              <div class="col">Center, XIV</div>
+              <div class="col">XIV</div>
+              <div class="col">XIV</div>
+              <div class="col">XIV</div>
             </div>
             <div class="row">
-              <div class="column">Army</div>
-              <div class="column">Army of the Ohio</div>
-              <div class="column">Army of the Ohio</div>
-              <div class="column">Army of the Cumberland</div>
-              <div class="column">Army of the Cumberland</div>
-              <div class="column">Army of the Cumberland</div>
-              <div class="column">Army of the Cumberland</div>
+              <div class="col">Army</div>
+              <div class="col">Army of the Ohio</div>
+              <div class="col">Army of the Ohio</div>
+              <div class="col">Army of the Cumberland</div>
+              <div class="col">Army of the Cumberland</div>
+              <div class="col">Army of the Cumberland</div>
+              <div class="col">Army of the Cumberland</div>
             </div>
             <div class="row">
-              <div class="column">Significant events</div>
-              <div class="column">Mustered in August 20, 1862 under command of Col. Albert S. Hall</div>
-              <div class="column">Battle of Perryville, KY, October 1862</div>
-              <div class="column">&nbsp;</div>
-              <div class="column">Stationed in Murfreeesboro, TN</div>
-              <div class="column">Battle of Chickamauga, September 1863</div>
-              <div class="column">Seige of Chattanooga; battle of Atlanta; march to the sea</div>
+              <div class="col">Significant events</div>
+              <div class="col">Mustered in August 20, 1862 under command of Col. Albert S. Hall</div>
+              <div class="col">Battle of Perryville, KY, October 1862</div>
+              <div class="col">&nbsp;</div>
+              <div class="col">Stationed in Murfreeesboro, TN</div>
+              <div class="col">Battle of Chickamauga, September 1863</div>
+              <div class="col">Seige of Chattanooga; battle of Atlanta; march to the sea</div>
             </div>
 
 
 
           </div><!--/.container -->
                 <?php include 'footer.php';?>
-                <!-- Bootstrap core JavaScript    ================================================== -->
-                <!-- Placed at the end of the document so the pages load faster -->
+                <!-- Bootstrap core JavaScript. Upgrade to V4 CDN    ================================================== -->
+                <!-- Placed at the end of the document so the pages load faster 
                 <script src="bootstrap-3.0.0/assets/js/jquery.js"></script>
-                <script src="bootstrap-3.0.0/dist/js/bootstrap.min.js"></script>
+                <script src="bootstrap-3.0.0/dist/js/bootstrap.min.js"></script>-->
+                <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
             </body>
           </html>
